ed519b07aeb299fcbf175b7b98d507f75ab12026
Kenawt <lim3rrol@gmail.com>
2020-06-23 09:35:17 +0800

    GameLose, Restart and additional functions.
    -Errol

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,14 +1,26 @@
 ﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //This script manages the behavior of individual obstacle
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] private float Speed = 3;
-
+    bool addedScore = false;
     void Update()
     {
+        if (Player.TrueScore <= 10)
+        {
+            Speed = 5;
+        }
+        else
+            Speed = 3;
+        if (transform.position.x <= 0 && addedScore == false)
+        {
+            addedScore = true;
+            Player.TrueScore++;
+        }
         if (transform.position.x <= -8)
             Destroy(gameObject);
         else
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    public Text Score;
+    public static int TrueScore;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        TrueScore = 0;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        Score.text = "SCORE : " + Player.TrueScore;
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+        pos.x = Mathf.Clamp(pos.x, 0.1f, 0.9f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y <= 6.6)
+        {
             thisAnimation.Play();
+            GetComponent<Rigidbody>().AddForce(transform.up * 5, ForceMode.Impulse);
+        }
+
+        if(transform.position.y>=6.7)
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+
+        if (transform.position.y <= -4.773493)
+            SceneManager.LoadScene("GameLose");
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+            SceneManager.LoadScene("GameLose");
     }
 }
